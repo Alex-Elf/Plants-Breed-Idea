e094390b01771dc67b2885e9ea82fe2986250370
Alex Popov <popov.alex.elf@gmail.com>
2020-02-27 21:32:29 +0200

    first comit

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant
+{
+	public const short parameterMax = 10;
+	public const short parameterMin = 0;
+
+	public Plant[] parents;
+	public short growSpeed;
+	public short productYield;
+
+}
